e601cdfe96ef30b15a073e7525c158911021ce83
xue160709 <xue160709@gmail.com>
2023-04-07 00:37:42 +0800

    Update URI.js

--- js/URI.js
+++ js/URI.js
@@ -6,6 +6,7 @@ console.log(code);
 const clientId = '52e170e1-ce8d-43b5-96c3-1573d1bf7a43';
 const clientSecret = 'secret_odBLZVfHGOAcufnFNkJrWXfpkFKDit0qd2GL8QEQh2a';
 const redirectUri = 'https://www.mix-copilot.com/';
+const base64Auth = btoa(`${clientId}:${clientSecret}`);
 
 const url = `https://api.notion.com/v1/oauth/token?grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}`;
 
@@ -14,17 +15,20 @@ fetch(url, {
     mode: 'no-cors',
     headers: {
         'Content-Type': 'application/json',
-        'Notion-Version': '2021-08-16'
-    }
+        'Authorization': `Basic ${base64Auth}`
+    },
+    body: JSON.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri
+    })
 }).then(response => {
     if (!response.ok) {
-        throw new Error('Failed to get access token');
+        throw new Error('Failed to get databases');
     }
     return response.json();
 }).then(data => {
-    const accessToken = data.access_token;
-    console.log(accessToken);
-    // Use the access token to call Notion API
+    console.log(data.results);
 }).catch(error => {
     console.error(error);
 });
