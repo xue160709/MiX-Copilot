b415dc1da9fef5ca5559b7fd3280ae350d091820
xue160709 <xue160709@gmail.com>
2023-04-07 13:49:29 +0800

    Update URI.js

--- js/URI.js
+++ js/URI.js
@@ -8,29 +8,52 @@ const clientSecret = 'secret_odBLZVfHGOAcufnFNkJrWXfpkFKDit0qd2GL8QEQh2a';
 const redirectUri = 'https://www.mix-copilot.com/';
 const base64Auth = btoa(`${clientId}:${clientSecret}`);
 
-fetch("https://api.notion.com/v1/oauth/token", {
-    method: "POST",
+// fetch("https://api.notion.com/v1/oauth/token", {
+//     method: "POST",
+//     mode: 'no-cors',
+//     headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+//     },
+//     body: JSON.stringify({
+//         grant_type: "authorization_code",
+//         code: authorization_code,
+//         redirect_uri: redirectUri,
+//     }),
+// })
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error("Network response was not ok");
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         const access_token = data.access_token;
+//         // 使用 access_token 来访问 Notion API
+//     })
+//     .catch((error) => {
+//         console.error("There was a problem fetching the access token:", error);
+//     });
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Basic ${btoa(`${clientId}:${clientSecret}`)}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+    "grant_type": "authorization_code",
+    "code": authorization_code,
+    "redirect_uri": redirectUri
+});
+
+var requestOptions = {
+    method: 'POST',
     mode: 'no-cors',
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-    },
-    body: JSON.stringify({
-        grant_type: "authorization_code",
-        code: authorization_code,
-        redirect_uri: redirectUri,
-    }),
-})
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-    })
-    .then((data) => {
-        const access_token = data.access_token;
-        // 使用 access_token 来访问 Notion API
-    })
-    .catch((error) => {
-        console.error("There was a problem fetching the access token:", error);
-    });
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+fetch("https://api.notion.com/v1/oauth/token", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
