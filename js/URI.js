76cb70b83a3869a80504d033aa50cc3f052ccbac
xue160709 <xue160709@gmail.com>
2023-04-07 13:15:43 +0800

    Update URI.js

--- js/URI.js
+++ js/URI.js
@@ -1,34 +1,35 @@
 const redirect_url_response = window.location.href;
 const regex = /code=([\w-]+)&/g;
 const match = regex.exec(redirect_url_response);
-const code = match[1];
-console.log(code);
+const authorization_code = match[1];
+console.log(authorization_code);
 const clientId = '52e170e1-ce8d-43b5-96c3-1573d1bf7a43';
 const clientSecret = 'secret_odBLZVfHGOAcufnFNkJrWXfpkFKDit0qd2GL8QEQh2a';
 const redirectUri = 'https://www.mix-copilot.com/';
 const base64Auth = btoa(`${clientId}:${clientSecret}`);
 
-const url = `https://api.notion.com/v1/oauth/token?grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}`;
-
-fetch(url, {
-    method: 'POST',
-    mode: 'no-cors',
+fetch("https://api.notion.com/v1/oauth/token", {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${base64Auth}`
+        "Content-Type": "application/json",
+        Authorization: `Basic ${btoa(`${client_id}:${client_secret}`)}`,
     },
     body: JSON.stringify({
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUri
+        grant_type: "authorization_code",
+        code: authorization_code,
+        redirect_uri: redirectUri,
+    }),
+})
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
     })
-}).then(response => {
-    if (!response.ok) {
-        throw new Error('Failed to get databases');
-    }
-    return response.json();
-}).then(data => {
-    console.log(data.results);
-}).catch(error => {
-    console.error(error);
-});
+    .then((data) => {
+        const access_token = data.access_token;
+        // 使用 access_token 来访问 Notion API
+    })
+    .catch((error) => {
+        console.error("There was a problem fetching the access token:", error);
+    });
